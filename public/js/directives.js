5dc3052ecdd784cf121b9d3c7fde1ff6142a83cc
Murali <muraliavarma@gmail.com>
2013-03-09 21:21:23 -0500

    Added isolate scope to attributes (AngularJS)

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,22 +1,53 @@
 app.directive('sceneChain', function() {
 	return {
 		restrict: 'A',
+		scope: {
+			attribute: '=',
+			pickedObject: '=',
+			item: '='
+		},
 		link: function(scope, element, attrs) {
-			var attr = scope.pickedObject;
-			for (var i = 0; i < scope.attribute.sceneChain.length; i++) {
-				attr = attr[scope.attribute.sceneChain[i]];
-			}
-			scope.sceneAttribute = attr[scope.item] * (1.0/(scope.attribute.multiplier? parseFloat(scope.attribute.multiplier): 1));
+			getAttrs();
 
 			scope.$watch('sceneAttribute', function() {
+				// console.log(scope.attribute);
+				setAttrs();
+			});
+
+			scope.$watch('pickedObject', function() {
+				getAttrs();
+			});
+
+			function getAttrs() {
+				var multiplier = (scope.attribute.multiplier? parseFloat(scope.attribute.multiplier): 1);
 				var attr = scope.pickedObject;
 				for (var i = 0; i < scope.attribute.sceneChain.length; i++) {
 					attr = attr[scope.attribute.sceneChain[i]];
 				}
-				attr[scope.item] = scope.sceneAttribute * (scope.attribute.multiplier? parseFloat(scope.attribute.multiplier): 1);
-				render();
-			});
+				if (scope.item) {
+					attr = attr[scope.item];
+				}
+				scope.sceneAttribute = scope.attribute.type == 'text' ? attr : attr / multiplier;
+			}
 
+			function setAttrs() {
+				var multiplier = (scope.attribute.multiplier? parseFloat(scope.attribute.multiplier): 1);
+				var attr = scope.pickedObject;
+				if (!attr) {
+					return;
+				}
+				var chain = scope.attribute.sceneChain;
+				for (var i = 0; i < chain.length - 1; i++) {
+					attr = attr[chain[i]];
+				}
+				if (scope.item) {
+					attr[chain[chain.length-1]][scope.item] = scope.sceneAttribute * multiplier;
+				}
+				else if (scope.attribute.type == 'text') {
+					attr[chain[chain.length-1]] = scope.sceneAttribute;
+				}
+				render();
+			}
 		}
 	}
 })
